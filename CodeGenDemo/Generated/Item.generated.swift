6658033628dbd096c1942fe39e9f1cda838d8bc0
Olivier Halligon <olivier.halligon@babylonhealth.com>
2019-06-18 02:41:54 +0200

    Stage 4a: Our first Item.stencil Sourcery template

---
+++ CodeGenDemo/Generated/Item.generated.swift
@@ -0,0 +1,8 @@
+// Generated using Sourcery 0.16.1 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+// All Models...
+
+// - Model type: Film
+// - Model type: Person
+// - Model type: Ship
