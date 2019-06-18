41b316ee44b14b8bb1e9da1f14a8e02aebce5917
Olivier Halligon <olivier.halligon@babylonhealth.com>
2019-06-18 02:57:53 +0200

    Stage 4e: Item+Encodable

--- CodeGenDemo/Generated/Item.generated.swift
+++ CodeGenDemo/Generated/Item.generated.swift
@@ -47,3 +47,23 @@ extension Item {
         }
     }
 }
+
+extension Item: Encodable {
+    enum CodingKeys: CodingKey {
+        case type, item
+    }
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        switch self {
+        case .film(let film):
+            try container.encode("film", forKey: .type)
+            try container.encode(film, forKey: .item)
+        case .person(let person):
+            try container.encode("person", forKey: .type)
+            try container.encode(person, forKey: .item)
+        case .ship(let ship):
+            try container.encode("ship", forKey: .type)
+            try container.encode(ship, forKey: .item)
+        }
+    }
+}
